dd6d581686e11424e19662414b416e6f9a74352e
Drysb <bobby.drysdale9@gmail.com>
2024-02-16 22:56:31 -0500

    Starting to build out the Window class

--- main.py
+++ main.py
@@ -3,4 +3,27 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        pass
+        self.root = tk.Tk()
+        self.root.title("Solve the Maze")
+
+        self.canvas_w = Canvas()
+        self.canvas_w.pack()
+        self.window_run = False
+
+        self.root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.root.update_idletasks()
+        self.root.update()
+    
+    def wait_for_close(self):
+        self.window_run = True
+        while self.window_run == True:
+            self.redraw()
+
+    def close(self):
+        self.window_run = False
+
+def main(self):
+    win = Window(800, 600)
+    win.wait_for_close()
